96ef5016912f2df4c8453b6264a75788d3394c82
Alberto <albertovalidofuentes2001@gmail.com>
2023-03-07 09:04:50 +0000

    Initial commit

---
+++ server/src/main/java/com/proyecto/itg/ItgApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.itg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItgApplication.class, args);
+	}
+
+}
