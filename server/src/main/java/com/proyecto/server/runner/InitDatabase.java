1e02a50d99ba6312ad56ce4427ee970fdf8e9cf3
Joel Munuera Marrero <joel.munu@gmail.com>
2023-05-26 12:33:18 +0100

    Added user sample data

--- server/src/main/java/com/proyecto/server/runner/InitDatabase.java
+++ server/src/main/java/com/proyecto/server/runner/InitDatabase.java
@@ -1,5 +1,23 @@
 package com.proyecto.server.runner;
 
-public class InitDatabase {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.proyecto.server.entities.User;
+import com.proyecto.server.services.UserService;
+
+@Component
+public class InitDatabase implements CommandLineRunner{
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User("nombre", "fullname", 1.0f, 1.0f, 1, "male", "1234", 122, 122, 122);
+        userService.save(user);
+    }
+
     
 }
--- server/src/main/java/com/proyecto/server/services/UserService.java
+++ server/src/main/java/com/proyecto/server/services/UserService.java
@@ -1,10 +1,12 @@
 package com.proyecto.server.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.proyecto.server.entities.User;
 import com.proyecto.server.repository.UserRepository;
 
+@Service
 public class UserService {
 
     @Autowired
