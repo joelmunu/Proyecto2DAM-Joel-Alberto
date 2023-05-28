319d567c00ef40194ed2a8f770460697ca8512ce
Alberto <albertovalidofuentes2001@gmail.com>
2023-05-29 00:02:17 +0200

    Added login funcionality

--- client/components/LoginScreen.jsx
+++ client/components/LoginScreen.jsx
@@ -1,9 +1,52 @@
-import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from 'react-native';
 
 import YellowButton from './common/YellowButton';
+import { useState } from 'react';
 
 
 export default function LoginScreen() {
+    const [formData, setFormData] = useState({
+        "username": "",
+        "password": "",
+
+    })
+
+    const handleInputChange = (fieldName, value) => {
+        setFormData(prevState => ({
+            ...prevState,
+            [fieldName]: value,
+        }));
+        console.log(formData)
+    };
+
+    const login = () => {
+        if (
+            formData.username === '' ||
+            formData.password === ''
+        ) {
+            alert('All fields are required')
+        } else {
+            const url = 'http://192.168.0.27:8080/login'; // Reemplaza con la dirección IP y puerto correctos de tu servidor
+            console.log(JSON.stringify(formData))
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+                .then(response => {
+                    if (response.ok) {
+                        alert("Ha iniciado sesión")
+                        return response.json();
+                    } else {
+                        alert("Error al inciar sesion")
+                        throw new Error('Error en la solicitud');
+                    }
+                })
+        }
+    };
+
     return (
         <ImageBackground source={require('../assets/login.jpg')} resizeMode="cover" style={styles.image}>
             <View style={styles.loginContainer}>
@@ -12,16 +55,21 @@ export default function LoginScreen() {
                         <Text style={styles.text}>Username</Text>
                         <TextInput
                             style={styles.input}
+                            value={formData.username}
+                            onChangeText={value => handleInputChange('username', value)}
                         />
                     </View>
                     <View>
                         <Text style={styles.text}>Password</Text>
                         <TextInput
                             style={styles.input}
+                            value={formData.password}
+                            onChangeText={value => handleInputChange('password', value)}
                         />
                     </View>
                     <View style={styles.centerButton}>
-                        <YellowButton />
+                        <Button title='Login' style={styles.button} onPress={login}>
+                        </Button>
                     </View>
                 </View>
             </View>
@@ -85,5 +133,16 @@ const styles = StyleSheet.create({
         textDecorationLine: "underline",
         color: "#39393B",
         fontWeight: "bold"
-    }
+    },
+    button: {
+        backgroundColor: "#FF8000",
+        width: 200,
+        marginTop: 40,
+        padding: 20,
+        borderRadius: 20,
+        alignItems: "center",
+    },
+    centerButton: {
+        alignItems: "center"
+    },
 })
